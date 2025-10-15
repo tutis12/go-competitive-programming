d11b487601645a8868952f6c89621efd1462d704
Tautvydas Jasiūnas <tautsjasiunsas@gmail.com>
2025-10-15 08:03:49 +0300

    gpt code lol

--- generator/output/generated_main.go
+++ generator/output/generated_main.go
@@ -8,12 +8,6 @@ import (
 	"runtime"
 )
 
-const (
-	fromFile   = false
-	inputFile  = "input.txt"
-	outputFile = "output.txt"
-)
-
 func main() {
 	var stdin = &Reader{
 		File: os.Stdin,
@@ -41,7 +35,178 @@ func main() {
 	solveX(stdin, stdout)
 }
 
+type (
+	Reader struct {
+		File  *os.File
+		bytes [buffSize]byte
+		from  int
+		to    int
+	}
+
+	Writer struct {
+		File      *os.File
+		buffer    [buffSize]byte
+		intBuffer [maxIntSize]byte
+		used      int
+	}
+)
+
+const (
+	fromFile   = false
+	inputFile  = "input.txt"
+	outputFile = "output.txt"
+)
+
+func (w *Writer) WriteAll() {
+	n, err := w.File.Write(w.buffer[:w.used])
+	if n != w.used {
+		panic("failed to write: " + err.Error())
+	}
+	w.used = 0
+}
+
+func Recover() {
+	err := recover()
+	if err == nil {
+		return
+	}
+	defer os.Exit(13)
+
+	buf := make([]byte, 10000)
+	n := runtime.Stack(buf, false)
+	buf = buf[:n]
+	fmt.Fprintf(os.Stderr, "panic: %v\nstacktrace:\n%s", err, string(buf))
+}
+
 var solveX = solveC
+
+func (w *Writer) Fprintf(format string, a ...any) {
+	w.String(fmt.Sprintf(format, a...))
+}
+
+func (r *Reader) String() string {
+	res := []byte{}
+	afterWhite := false
+	if r.from == r.to {
+		if !r.read() {
+			return ""
+		}
+	}
+	for {
+		fr := r.from
+		for i := r.from; i < r.to; i++ {
+			r.from = i + 1
+			if isWhite(r.bytes[i]) {
+				if afterWhite {
+					res = append(res, r.bytes[fr:i]...)
+					return string(res)
+				} else {
+					fr = i + 1
+				}
+			} else {
+				afterWhite = true
+			}
+		}
+		res = append(res, r.bytes[fr:r.to]...)
+		if !r.read() {
+			break
+		}
+	}
+	return string(res)
+}
+
+func (w *Writer) String(s string) {
+	fr := 0
+	to := len(s)
+	for fr < to {
+		left := buffSize - w.used
+		toCopy := min(left, to-fr)
+		copy(w.buffer[w.used:w.used+toCopy], s[fr:fr+toCopy])
+		fr += toCopy
+		w.used += toCopy
+		if w.used >= buffSize-maxIntSize {
+			w.WriteAll()
+		}
+	}
+}
+
+func (r *Reader) read() bool {
+	n, _ := r.File.Read(r.bytes[:])
+	if n == 0 {
+		return false
+	} else {
+		r.from = 0
+		r.to = n
+		return true
+	}
+}
+
+func isWhite(c byte) bool {
+	return c == ' ' || c == '\n' || c == '\r' || c == '\t'
+}
+
+const (
+	buffSize = 100000
+
+	maxIntSize = 128
+)
+
+type (
+	STG1 struct {
+		log2n      int
+		n          int
+		arr        []nodeG1
+		zeroValue  stValue
+		zeroUpdate lazy
+	}
+	lazy struct {
+		addDP int
+	}
+
+	nodeG1 struct {
+		value  stValue
+		update lazy
+	}
+	stValue struct {
+		minA  int
+		minDP int
+	}
+)
+
+func IsPowerOf2G1(x int) bool {
+	x64 := uint64(x)
+	return x64 != 0 && (x64&(x64-1)) == 0
+}
+
+func NewSTG1(
+	init func(int) stValue,
+	size int,
+	zeroValue stValue,
+	zeroUpdate lazy,
+) *STG1 {
+	if size <= 0 {
+		panic("size must be positive")
+	}
+	log2n := Log2CeilG1(size)
+	n := 1 << log2n
+	arr := make([]nodeG1, 2*n)
+	for i := range size {
+		arr[n+i] = nodeG1{init(i), zeroUpdate}
+	}
+	for i := size; i < n; i++ {
+		arr[n+i] = nodeG1{zeroValue, zeroUpdate}
+	}
+	for i := n - 1; i > 0; i-- {
+		arr[i] = nodeG1{arr[2*i].value.Merge(arr[2*i+1].value), zeroUpdate}
+	}
+	return &STG1{
+		log2n:      log2n,
+		n:          n,
+		arr:        arr,
+		zeroValue:  zeroValue,
+		zeroUpdate: zeroUpdate,
+	}
+}
 
 func solveC(
 	stdin *Reader,
@@ -51,27 +216,6 @@ func solveC(
 	for range t {
 		solveTestC(stdin, stdout)
 	}
-}
-
-type stValue struct {
-	minA  int
-	minDP int
-}
-
-type lazy struct {
-	addDP int
-}
-
-func (a stValue) Merge(b stValue) stValue {
-	return stValue{minA: min(a.minA, b.minA), minDP: min(a.minDP, b.minDP)}
-}
-
-func (up lazy) ApplyUpdate(val *stValue) {
-	val.minDP += up.addDP
-}
-
-func (top lazy) Push(existing *lazy) {
-	existing.addDP += top.addDP
 }
 
 func solveTestC(
@@ -119,284 +263,25 @@ func solveTestC(
 	stdout.Int(dp[n-1], '\n')
 }
 
-const (
-	mod = 1000000007
-)
-
-type Reader struct {
-	File  *os.File
-	bytes [buffSize]byte
-	from  int
-	to    int
+func Log2CeilG1(x int) int {
+	x64 := uint64(x)
+	if x64 == 0 {
+		panic("Log2(0) is undefined")
+	}
+	if x64 == 1 {
+		return 0
+	}
+	return 1 + Log2FloorG1(x64-1)
 }
 
-func (r *Reader) read() bool {
-	n, _ := r.File.Read(r.bytes[:])
-	if n == 0 {
-		return false
-	} else {
-		r.from = 0
-		r.to = n
-		return true
+func Log2FloorG1(x uint64) int {
+	x64 := uint64(x)
+	if x64 == 0 {
+		panic("Log2(0) is undefined")
 	}
+	return 63 - bits.LeadingZeros64(x64)
 }
 
-func (r *Reader) peek() (byte, bool) {
-	if r.from == r.to {
-		if !r.read() {
-			return 0, false
-		}
-	}
-	return r.bytes[r.from], true
-}
-
-func (r *Reader) seek() {
-	r.from++
-}
-
-func (r *Reader) Int() int {
-	pos := true
-	n := 0
-	for {
-		c, ok := r.peek()
-		if !ok {
-			return 0
-		}
-		r.seek()
-		if '0' <= c && c <= '9' {
-			n = int(c - '0')
-			break
-		}
-		if c == '-' {
-			pos = false
-			break
-		}
-	}
-
-	for {
-		c, ok := r.peek()
-		if !ok {
-			break
-		}
-		r.seek()
-		if c < '0' || c > '9' {
-			break
-		}
-		n = n*10 + int(c-'0')
-	}
-	if !pos {
-		n = -n
-	}
-	return n
-}
-
-func (r *Reader) Ints(n int, firstIndex int) []int {
-	a := make([]int, n+firstIndex)
-	for i := range n {
-		a[i+firstIndex] = r.Int()
-	}
-	return a
-}
-
-func isWhite(c byte) bool {
-	return c == ' ' || c == '\n' || c == '\r' || c == '\t'
-}
-
-func (r *Reader) String() string {
-	res := []byte{}
-	afterWhite := false
-	if r.from == r.to {
-		if !r.read() {
-			return ""
-		}
-	}
-	for {
-		fr := r.from
-		for i := r.from; i < r.to; i++ {
-			r.from = i + 1
-			if isWhite(r.bytes[i]) {
-				if afterWhite {
-					res = append(res, r.bytes[fr:i]...)
-					return string(res)
-				} else {
-					fr = i + 1
-				}
-			} else {
-				afterWhite = true
-			}
-		}
-		res = append(res, r.bytes[fr:r.to]...)
-		if !r.read() {
-			break
-		}
-	}
-	return string(res)
-}
-
-const buffSize = 100000
-
-const maxIntSize = 128
-
-type Writer struct {
-	File      *os.File
-	buffer    [buffSize]byte
-	intBuffer [maxIntSize]byte
-	used      int
-}
-
-func (w *Writer) WriteAll() {
-	n, err := w.File.Write(w.buffer[:w.used])
-	if n != w.used {
-		panic("failed to write: " + err.Error())
-	}
-	w.used = 0
-}
-
-func (w *Writer) bytes(c []byte) {
-	if len(c) >= maxIntSize {
-		panic("bytes too long")
-	}
-	copy(w.buffer[w.used:], c)
-	w.used += len(c)
-	if w.used >= buffSize-maxIntSize {
-		w.WriteAll()
-	}
-}
-
-func (w *Writer) String(s string) {
-	fr := 0
-	to := len(s)
-	for fr < to {
-		left := buffSize - w.used
-		toCopy := min(left, to-fr)
-		copy(w.buffer[w.used:w.used+toCopy], s[fr:fr+toCopy])
-		fr += toCopy
-		w.used += toCopy
-		if w.used >= buffSize-maxIntSize {
-			w.WriteAll()
-		}
-	}
-}
-
-func (w *Writer) Int(value int, c byte) {
-	pos := true
-	var n uint
-	if value < 0 {
-		pos = false
-		n = uint(-value)
-	} else {
-		n = uint(value)
-	}
-	i := maxIntSize - 1
-	w.intBuffer[i] = c
-	i--
-	if n == 0 {
-		w.intBuffer[i] = '0'
-		i--
-	}
-	for n != 0 {
-		w.intBuffer[i] = '0' + byte(n%10)
-		n /= 10
-		i--
-	}
-	if !pos {
-		w.intBuffer[i] = '-'
-		i--
-	}
-	w.bytes(w.intBuffer[i+1:])
-}
-
-func (w *Writer) Fprintf(format string, a ...any) {
-	w.String(fmt.Sprintf(format, a...))
-}
-
-func (w *Writer) Char(c byte) {
-	w.bytes([]byte{c})
-}
-
-func (w *Writer) Ints(n []int, sep byte, end byte) {
-	if len(n) == 0 {
-		w.Char(end)
-	}
-	for i, v := range n {
-		if i != len(n)-1 {
-			w.Int(v, sep)
-		} else {
-			w.Int(v, end)
-		}
-	}
-}
-
-func Recover() {
-	err := recover()
-	if err == nil {
-		return
-	}
-	defer os.Exit(13)
-
-	buf := make([]byte, 10000)
-	n := runtime.Stack(buf, false)
-	buf = buf[:n]
-	fmt.Fprintf(os.Stderr, "panic: %v\nstacktrace:\n%s", err, string(buf))
-}
-
-type STG1 struct {
-	log2n      int
-	n          int
-	arr        []nodeG1
-	zeroValue  stValue
-	zeroUpdate lazy
-}
-type nodeG1 struct {
-	value  stValue
-	update lazy
-}
-
-func (st STG1) SetValue(
-	i int,
-	val stValue,
-) {
-	if i < 0 || i >= st.n {
-		panic("index out of bounds")
-	}
-	st.pushUpdates(i)
-	st.arr[i+st.n].value = val
-	st.arr[i+st.n].update = st.zeroUpdate
-	st.rebuild(i)
-}
-
-func (st STG1) Get(
-	l, r int,
-) stValue {
-	l = max(l, 0)
-	r = min(r, st.n-1)
-	if l > r {
-		return st.zeroValue
-	}
-	st.pushUpdates(l)
-	st.pushUpdates(r)
-	summedL := st.zeroValue
-	summedR := st.zeroValue
-	l, r = l+st.n, r+st.n
-	for l <= r {
-		if l%2 == 1 {
-
-			(st.arr[l].update).ApplyUpdate(&st.arr[l].value)
-			summedL = summedL.Merge(st.arr[l].value)
-			l = l/2 + 1
-		} else {
-			l = l / 2
-		}
-		if r%2 == 0 {
-			(st.arr[r].update).ApplyUpdate(&st.arr[r].value)
-			summedR = st.arr[r].value.Merge(summedR)
-			r = r/2 - 1
-		} else {
-			r = r / 2
-		}
-	}
-	return summedL.Merge(summedR)
-}
 func (st STG1) LongestRangeWherePredicate(
 	rMax int,
 	predicate func(stValue) bool,
@@ -446,6 +331,20 @@ func (st STG1) LongestRangeWherePredicate(
 	}
 	return i - st.n + 1, true
 }
+
+func (st STG1) SetValue(
+	i int,
+	val stValue,
+) {
+	if i < 0 || i >= st.n {
+		panic("index out of bounds")
+	}
+	st.pushUpdates(i)
+	st.arr[i+st.n].value = val
+	st.arr[i+st.n].update = st.zeroUpdate
+	st.rebuild(i)
+}
+
 func (st STG1) pushUpdates(
 	i int,
 ) {
@@ -463,6 +362,109 @@ func (st STG1) pushUpdates(
 	(st.arr[i].update).ApplyUpdate(&st.arr[i].value)
 	st.arr[i].update = st.zeroUpdate
 }
+
+func (w *Writer) Char(c byte) {
+	w.bytes([]byte{c})
+}
+
+func (r *Reader) Int() int {
+	pos := true
+	n := 0
+	for {
+		c, ok := r.peek()
+		if !ok {
+			return 0
+		}
+		r.seek()
+		if '0' <= c && c <= '9' {
+			n = int(c - '0')
+			break
+		}
+		if c == '-' {
+			pos = false
+			break
+		}
+	}
+
+	for {
+		c, ok := r.peek()
+		if !ok {
+			break
+		}
+		r.seek()
+		if c < '0' || c > '9' {
+			break
+		}
+		n = n*10 + int(c-'0')
+	}
+	if !pos {
+		n = -n
+	}
+	return n
+}
+
+func (w *Writer) Int(value int, c byte) {
+	pos := true
+	var n uint
+	if value < 0 {
+		pos = false
+		n = uint(-value)
+	} else {
+		n = uint(value)
+	}
+	i := maxIntSize - 1
+	w.intBuffer[i] = c
+	i--
+	if n == 0 {
+		w.intBuffer[i] = '0'
+		i--
+	}
+	for n != 0 {
+		w.intBuffer[i] = '0' + byte(n%10)
+		n /= 10
+		i--
+	}
+	if !pos {
+		w.intBuffer[i] = '-'
+		i--
+	}
+	w.bytes(w.intBuffer[i+1:])
+}
+
+func (r *Reader) Ints(n int, firstIndex int) []int {
+	a := make([]int, n+firstIndex)
+	for i := range n {
+		a[i+firstIndex] = r.Int()
+	}
+	return a
+}
+
+func (w *Writer) Ints(n []int, sep byte, end byte) {
+	if len(n) == 0 {
+		w.Char(end)
+	}
+	for i, v := range n {
+		if i != len(n)-1 {
+			w.Int(v, sep)
+		} else {
+			w.Int(v, end)
+		}
+	}
+}
+
+func (top lazy) Push(existing *lazy) {
+	existing.addDP += top.addDP
+}
+
+func (r *Reader) peek() (byte, bool) {
+	if r.from == r.to {
+		if !r.read() {
+			return 0, false
+		}
+	}
+	return r.bytes[r.from], true
+}
+
 func (st STG1) rebuild(
 	i int,
 ) {
@@ -478,54 +480,58 @@ func (st STG1) rebuild(
 	}
 }
 
-func NewSTG1(
-	init func(int) stValue,
-	size int,
-	zeroValue stValue,
-	zeroUpdate lazy,
-) *STG1 {
-	if size <= 0 {
-		panic("size must be positive")
-	}
-	log2n := Log2CeilG1(size)
-	n := 1 << log2n
-	arr := make([]nodeG1, 2*n)
-	for i := range size {
-		arr[n+i] = nodeG1{init(i), zeroUpdate}
-	}
-	for i := size; i < n; i++ {
-		arr[n+i] = nodeG1{zeroValue, zeroUpdate}
-	}
-	for i := n - 1; i > 0; i-- {
-		arr[i] = nodeG1{arr[2*i].value.Merge(arr[2*i+1].value), zeroUpdate}
-	}
-	return &STG1{
-		log2n:      log2n,
-		n:          n,
-		arr:        arr,
-		zeroValue:  zeroValue,
-		zeroUpdate: zeroUpdate,
-	}
-}
-func Log2FloorG1(x uint64) int {
-	x64 := uint64(x)
-	if x64 == 0 {
-		panic("Log2(0) is undefined")
-	}
-	return 63 - bits.LeadingZeros64(x64)
-}
-func Log2CeilG1(x int) int {
-	x64 := uint64(x)
-	if x64 == 0 {
-		panic("Log2(0) is undefined")
-	}
-	if x64 == 1 {
-		return 0
-	}
-	return 1 + Log2FloorG1(x64-1)
-}
-func IsPowerOf2G1(x int) bool {
-	x64 := uint64(x)
-	return x64 != 0 && (x64&(x64-1)) == 0
+func (r *Reader) seek() {
+	r.from++
 }
 
+func (a stValue) Merge(b stValue) stValue {
+	return stValue{minA: min(a.minA, b.minA), minDP: min(a.minDP, b.minDP)}
+}
+
+func (w *Writer) bytes(c []byte) {
+	if len(c) >= maxIntSize {
+		panic("bytes too long")
+	}
+	copy(w.buffer[w.used:], c)
+	w.used += len(c)
+	if w.used >= buffSize-maxIntSize {
+		w.WriteAll()
+	}
+}
+
+func (up lazy) ApplyUpdate(val *stValue) {
+	val.minDP += up.addDP
+}
+
+func (st STG1) Get(
+	l, r int,
+) stValue {
+	l = max(l, 0)
+	r = min(r, st.n-1)
+	if l > r {
+		return st.zeroValue
+	}
+	st.pushUpdates(l)
+	st.pushUpdates(r)
+	summedL := st.zeroValue
+	summedR := st.zeroValue
+	l, r = l+st.n, r+st.n
+	for l <= r {
+		if l%2 == 1 {
+
+			(st.arr[l].update).ApplyUpdate(&st.arr[l].value)
+			summedL = summedL.Merge(st.arr[l].value)
+			l = l/2 + 1
+		} else {
+			l = l / 2
+		}
+		if r%2 == 0 {
+			(st.arr[r].update).ApplyUpdate(&st.arr[r].value)
+			summedR = st.arr[r].value.Merge(summedR)
+			r = r/2 - 1
+		} else {
+			r = r / 2
+		}
+	}
+	return summedL.Merge(summedR)
+}
--- generator/remove_unused_code.go
+++ generator/remove_unused_code.go
@@ -6,6 +6,7 @@ import (
 	"go/parser"
 	"go/printer"
 	"go/token"
+	"strings"
 )
 
 // RemoveUnusedCode aggressively removes top-level types, functions, and methods
@@ -28,6 +29,8 @@ func RemoveUnusedCode(src []byte) []byte {
 	methodDecls := map[string][]*ast.FuncDecl{}
 	// Type declarations by name
 	typeDecls := map[string]*ast.TypeSpec{}
+	// Global value/const declarations by name (single identifier specs only)
+	valueDecls := map[string]*ast.ValueSpec{}
 
 	for _, d := range file.Decls {
 		if fd, ok := d.(*ast.FuncDecl); ok {
@@ -42,6 +45,16 @@ func RemoveUnusedCode(src []byte) []byte {
 			for _, sp := range gd.Specs {
 				if ts, ok2 := sp.(*ast.TypeSpec); ok2 {
 					typeDecls[ts.Name.Name] = ts
+				}
+			}
+			continue
+		}
+		if gd, ok := d.(*ast.GenDecl); ok && (gd.Tok == token.CONST || gd.Tok == token.VAR) {
+			for _, sp := range gd.Specs {
+				if vs, ok2 := sp.(*ast.ValueSpec); ok2 {
+					for _, nm := range vs.Names {
+						valueDecls[nm.Name] = vs
+					}
 				}
 			}
 		}
@@ -73,6 +86,7 @@ func RemoveUnusedCode(src []byte) []byte {
 	reachableFuncs := map[string]bool{}
 	reachableMethods := map[string]bool{}
 	reachableTypes := map[string]bool{}
+	reachableValues := map[string]bool{}
 
 	queue := []string{}
 	for f := range rootFuncs {
@@ -145,6 +159,11 @@ func RemoveUnusedCode(src []byte) []byte {
 					for _, name := range extractTypeIdents(x.Type) {
 						reachableTypes[name] = true
 					}
+				}
+			case *ast.Ident:
+				// Track identifier usage for global value/const declarations
+				if _, ok := valueDecls[x.Name]; ok {
+					reachableValues[x.Name] = true
 				}
 			}
 			return true
@@ -276,6 +295,28 @@ func RemoveUnusedCode(src []byte) []byte {
 			}
 			continue
 		}
+		if gd, ok := d.(*ast.GenDecl); ok && (gd.Tok == token.CONST || gd.Tok == token.VAR) {
+			var keptSpecs []ast.Spec
+			for _, sp := range gd.Specs {
+				if vs, ok2 := sp.(*ast.ValueSpec); ok2 {
+					keep := false
+					for _, nm := range vs.Names {
+						if reachableValues[nm.Name] {
+							keep = true
+							break
+						}
+					}
+					if keep {
+						keptSpecs = append(keptSpecs, sp)
+					}
+				}
+			}
+			if len(keptSpecs) > 0 {
+				gd.Specs = keptSpecs
+				newDecls = append(newDecls, gd)
+			}
+			continue
+		}
 		// Retain non-type declarations (vars/consts) as they may influence logic or root detection; optional pruning could be added.
 		newDecls = append(newDecls, d)
 	}
@@ -293,10 +334,16 @@ func RemoveUnusedCode(src []byte) []byte {
 		}
 	}
 
-	var out bytes.Buffer
-	_ = printer.Fprint(&out, fset, file)
-	return out.Bytes()
+	file.Decls = SortDeclarations(file.Decls)
+
+	ordered := buildPreorderSource(file, fset)
+	ordered = collapseBlockBlankLines(ordered, "const (")
+	ordered = collapseBlockBlankLines(ordered, "var (")
+	ordered = ensureFunctionGaps(ordered)
+	return []byte(ordered)
 }
+
+// END RemoveUnusedCode
 
 // extractTypeIdents returns identifier names referenced in a type expression.
 func extractTypeIdents(e ast.Expr) []string {
@@ -324,3 +371,394 @@ func isBuiltin(name string) bool {
 }
 
 // (Optional) helper to debug sets; unused but handy for future tweaks.
+
+// collapseBlockBlankLines removes extra blank lines inside a parenthesized decl block.
+func collapseBlockBlankLines(src, prefix string) string {
+	idx := strings.Index(src, prefix)
+	if idx == -1 {
+		return src
+	}
+	start := idx + len(prefix)
+	depth := 1
+	for i := start; i < len(src); i++ {
+		c := src[i]
+		if c == '(' {
+			depth++
+		} else if c == ')' {
+			depth--
+			if depth == 0 {
+				block := src[idx : i+1]
+				lines := strings.Split(block, "\n")
+				var cleaned []string
+				for _, ln := range lines {
+					if strings.TrimSpace(ln) == "" { // drop all blank lines inside block
+						continue
+					}
+					cleaned = append(cleaned, ln)
+				}
+				newBlock := strings.Join(cleaned, "\n")
+				return src[:idx] + newBlock + src[i+1:]
+			}
+		}
+	}
+	return src
+}
+
+// buildPreorderSource orders declarations in a pre-order traversal starting from main
+// following first usage of functions, methods, types, vars, consts as they appear.
+func buildPreorderSource(file *ast.File, fset *token.FileSet) string {
+	// Collect declarations
+	funcDecls := map[string]*ast.FuncDecl{}
+	methodDecls := map[string][]*ast.FuncDecl{}
+	typeDecls := map[string]*ast.TypeSpec{}
+	valueDecls := map[string]struct {
+		vs  *ast.ValueSpec
+		tok token.Token
+	}{}
+	var mainDecl *ast.FuncDecl
+	var imports []*ast.GenDecl
+	for _, d := range file.Decls {
+		switch x := d.(type) {
+		case *ast.FuncDecl:
+			if x.Recv == nil {
+				if x.Name.Name == "main" {
+					mainDecl = x
+				}
+				funcDecls[x.Name.Name] = x
+			} else {
+				methodDecls[x.Name.Name] = append(methodDecls[x.Name.Name], x)
+			}
+		case *ast.GenDecl:
+			if x.Tok == token.IMPORT {
+				imports = append(imports, x)
+			}
+			if x.Tok == token.TYPE {
+				for _, sp := range x.Specs {
+					if ts, ok := sp.(*ast.TypeSpec); ok {
+						typeDecls[ts.Name.Name] = ts
+					}
+				}
+			}
+			if x.Tok == token.CONST || x.Tok == token.VAR {
+				for _, sp := range x.Specs {
+					if vs, ok := sp.(*ast.ValueSpec); ok {
+						for _, nm := range vs.Names {
+							valueDecls[nm.Name] = struct {
+								vs  *ast.ValueSpec
+								tok token.Token
+							}{vs, x.Tok}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	visited := map[string]bool{}
+	order := []string{}
+	stack := []string{}
+	if mainDecl != nil {
+		stack = append(stack, "main")
+	}
+
+	addName := func(name string) {
+		if name == "main" { // already handled explicitly
+			return
+		}
+		if visited[name] {
+			return
+		}
+		visited[name] = true
+		order = append(order, name)
+		// Depth-first traversal: push onto stack for further expansion if it has a body
+		if funcDecls[name] != nil || len(methodDecls[name]) > 0 {
+			stack = append(stack, name)
+		}
+	}
+
+	extractUsed := func(fd *ast.FuncDecl) []string {
+		used := []string{}
+		if fd == nil || fd.Body == nil {
+			return used
+		}
+		seenLocal := map[string]bool{}
+		ast.Inspect(fd.Body, func(n ast.Node) bool {
+			switch x := n.(type) {
+			case *ast.CallExpr:
+				switch fun := x.Fun.(type) {
+				case *ast.Ident:
+					if funcDecls[fun.Name] != nil && !seenLocal[fun.Name] {
+						used = append(used, fun.Name)
+						seenLocal[fun.Name] = true
+					}
+				case *ast.SelectorExpr:
+					if methodDecls[fun.Sel.Name] != nil && !seenLocal[fun.Sel.Name] {
+						used = append(used, fun.Sel.Name)
+						seenLocal[fun.Sel.Name] = true
+					}
+				}
+			case *ast.CompositeLit:
+				for _, tn := range extractTypeIdents(x.Type) {
+					if typeDecls[tn] != nil && !seenLocal[tn] {
+						used = append(used, tn)
+						seenLocal[tn] = true
+					}
+				}
+			case *ast.TypeAssertExpr:
+				for _, tn := range extractTypeIdents(x.Type) {
+					if typeDecls[tn] != nil && !seenLocal[tn] {
+						used = append(used, tn)
+						seenLocal[tn] = true
+					}
+				}
+			case *ast.UnaryExpr:
+				if cl, ok := x.X.(*ast.CompositeLit); ok {
+					for _, tn := range extractTypeIdents(cl.Type) {
+						if typeDecls[tn] != nil && !seenLocal[tn] {
+							used = append(used, tn)
+							seenLocal[tn] = true
+						}
+					}
+				}
+			case *ast.Ident:
+				if valueDecls[x.Name].vs != nil && !seenLocal[x.Name] {
+					used = append(used, x.Name)
+					seenLocal[x.Name] = true
+				}
+			case *ast.ValueSpec:
+				if x.Type != nil {
+					for _, tn := range extractTypeIdents(x.Type) {
+						if typeDecls[tn] != nil && !seenLocal[tn] {
+							used = append(used, tn)
+							seenLocal[tn] = true
+						}
+					}
+				}
+			}
+			return true
+		})
+		return used
+	}
+
+	for len(stack) > 0 {
+		cur := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if cur == "main" {
+			if !visited[cur] {
+				visited[cur] = true
+				// main at top, but we don't add to order list (printed separately)
+				for _, u := range extractUsed(mainDecl) {
+					addName(u)
+				}
+			}
+			continue
+		}
+		// Functions
+		if fd := funcDecls[cur]; fd != nil {
+			for _, u := range extractUsed(fd) {
+				addName(u)
+			}
+		}
+		// Methods (multiple with same name)
+		if mds := methodDecls[cur]; len(mds) > 0 {
+			for _, md := range mds {
+				for _, u := range extractUsed(md) {
+					addName(u)
+				}
+			}
+		}
+	}
+
+	// Build output
+	printNode := func(n ast.Node) string { var b bytes.Buffer; _ = printer.Fprint(&b, fset, n); return b.String() }
+
+	type emission struct {
+		kind token.Token // TYPE, CONST, VAR or ILLEGAL for funcs/methods/package/import sentinel
+		spec ast.Spec    // for TYPE/CONST/VAR single specs
+		node ast.Node    // for funcs/methods/import blocks
+	}
+	var emits []emission
+
+	var packageHeader bytes.Buffer
+	packageHeader.WriteString("package " + file.Name.Name + "\n\n")
+	for _, im := range imports {
+		packageHeader.WriteString(printNode(im) + "\n")
+	}
+	if len(imports) > 0 {
+		packageHeader.WriteString("\n")
+	}
+	if mainDecl != nil {
+		packageHeader.WriteString(printNode(mainDecl) + "\n\n")
+	}
+
+	printed := map[string]bool{"main": true}
+	emittedValueSpec := map[*ast.ValueSpec]bool{}
+
+	// helper to add type spec ensuring no duplicate
+	addTypeSpec := func(ts *ast.TypeSpec) { emits = append(emits, emission{kind: token.TYPE, spec: ts}) }
+	addValueSpec := func(tok token.Token, vs *ast.ValueSpec) { emits = append(emits, emission{kind: tok, spec: vs}) }
+	addFunc := func(fd *ast.FuncDecl) { emits = append(emits, emission{kind: token.ILLEGAL, node: fd}) }
+
+	for _, name := range order {
+		if printed[name] {
+			continue
+		}
+		printed[name] = true
+		if ts := typeDecls[name]; ts != nil {
+			addTypeSpec(ts)
+			continue
+		}
+		if info, ok := valueDecls[name]; ok && info.vs != nil {
+			if !emittedValueSpec[info.vs] {
+				addValueSpec(info.tok, info.vs)
+				emittedValueSpec[info.vs] = true
+			}
+			continue
+		}
+		if fd := funcDecls[name]; fd != nil {
+			addFunc(fd)
+			continue
+		}
+		if mds := methodDecls[name]; len(mds) > 0 {
+			for _, md := range mds {
+				// receiver type first if not printed
+				if md.Recv != nil && len(md.Recv.List) > 0 {
+					recvType := md.Recv.List[0].Type
+					if se, ok := recvType.(*ast.StarExpr); ok {
+						recvType = se.X
+					}
+					if id, ok := recvType.(*ast.Ident); ok {
+						if !printed[id.Name] {
+							if ts := typeDecls[id.Name]; ts != nil {
+								addTypeSpec(ts)
+								printed[id.Name] = true
+							}
+						}
+					}
+				}
+				addFunc(md)
+			}
+			continue
+		}
+	}
+
+	appendDecl := func(name string) {
+		if printed[name] {
+			return
+		}
+		printed[name] = true
+		if ts := typeDecls[name]; ts != nil {
+			addTypeSpec(ts)
+			return
+		}
+		if info, ok := valueDecls[name]; ok && info.vs != nil {
+			if !emittedValueSpec[info.vs] {
+				addValueSpec(info.tok, info.vs)
+				emittedValueSpec[info.vs] = true
+			}
+			return
+		}
+		if fd := funcDecls[name]; fd != nil {
+			addFunc(fd)
+			return
+		}
+		if mds := methodDecls[name]; len(mds) > 0 {
+			for _, md := range mds {
+				if md.Recv != nil && len(md.Recv.List) > 0 {
+					recvType := md.Recv.List[0].Type
+					if se, ok := recvType.(*ast.StarExpr); ok {
+						recvType = se.X
+					}
+					if id, ok := recvType.(*ast.Ident); ok {
+						if !printed[id.Name] {
+							if ts := typeDecls[id.Name]; ts != nil {
+								addTypeSpec(ts)
+								printed[id.Name] = true
+							}
+						}
+					}
+				}
+				addFunc(md)
+			}
+			return
+		}
+	}
+
+	for name := range typeDecls {
+		appendDecl(name)
+	}
+	for name := range valueDecls {
+		appendDecl(name)
+	}
+	for name := range funcDecls {
+		appendDecl(name)
+	}
+	for name := range methodDecls {
+		appendDecl(name)
+	}
+
+	// Group consecutive specs of same kind (TYPE, CONST, VAR)
+	var out bytes.Buffer
+	out.WriteString(packageHeader.String())
+	i := 0
+	for i < len(emits) {
+		// groupable tokens
+		if emits[i].kind == token.TYPE || emits[i].kind == token.CONST || emits[i].kind == token.VAR {
+			kind := emits[i].kind
+			j := i
+			var specs []ast.Spec
+			for j < len(emits) && emits[j].kind == kind {
+				if emits[j].spec != nil {
+					specs = append(specs, emits[j].spec)
+					j++
+					continue
+				}
+				break // encounter non-spec emission, stop grouping
+			}
+			if len(specs) > 1 { // create multi-spec block
+				gd := &ast.GenDecl{Tok: kind, Specs: specs}
+				out.WriteString(printNode(gd) + "\n\n")
+				i = j
+				continue
+			} else if len(specs) == 1 { // single spec
+				gd := &ast.GenDecl{Tok: kind, Specs: specs}
+				out.WriteString(printNode(gd) + "\n\n")
+				i = j
+				continue
+			}
+		}
+		// non-groupable (func/method/import etc.)
+		if emits[i].node != nil {
+			out.WriteString(printNode(emits[i].node) + "\n\n")
+		}
+		i++
+	}
+
+	return out.String()
+}
+
+// ensureFunctionGaps inserts exactly one blank line before each top-level func/method
+// declaration (except the first function in the file) and removes duplicate blank lines
+// directly preceding functions.
+func ensureFunctionGaps(src string) string {
+	lines := strings.Split(src, "\n")
+	var out []string
+	funcCount := 0
+	for _, ln := range lines {
+		trim := strings.TrimSpace(ln)
+		isFunc := strings.HasPrefix(trim, "func ")
+		if isFunc {
+			// backtrack over trailing blank lines already emitted
+			for len(out) > 0 && strings.TrimSpace(out[len(out)-1]) == "" {
+				out = out[:len(out)-1]
+			}
+			if funcCount > 0 { // not first function
+				out = append(out, "")
+			}
+			funcCount++
+		}
+		out = append(out, ln)
+		// no-op: we removed prevNonEmptyIndex tracking
+	}
+	return strings.Join(out, "\n")
+}
